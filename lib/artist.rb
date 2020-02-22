8a8ae795da80f506eb500b041c4db302fa144891
Kwane Smith <smithkwane6@gmail.com>
2020-02-22 07:12:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,13 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
+  
   attr_accessor :name
   attr_reader :songs
-  extend Memorable::ClassMethods
+
 
   @@artists = []
 
