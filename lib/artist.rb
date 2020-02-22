d159a7c8d0bc250257b77487cf331ba38fe48118
Kwane Smith <smithkwane6@gmail.com>
2020-02-22 07:43:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,13 +27,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#  def self.reset_all
+#    self.all.clear
+#  end
 
-  def self.count
-    self.all.count
-  end
+#  def self.count
+#    self.all.count
+#  end
 
   def add_song(song)
     @songs << song
@@ -43,7 +43,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#
 end
