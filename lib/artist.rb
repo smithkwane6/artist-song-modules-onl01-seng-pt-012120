72a51f4e14c8b9bf5663dbc8546c40b502cdc0d9
Kwane Smith <smithkwane6@gmail.com>
2020-02-22 08:07:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,29 +19,29 @@ class Artist
     @songs = []
   end
 
-#  def self.find_by_name(name)
-#    @@artists.detect{|a| a.name == name}
-#  end
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def self.all
     @@artists
   end
 
-#  def self.reset_all
-#    self.all.clear
-#  end
+  def self.reset_all
+    self.all.clear
+  end
 
-#  def self.count
-#    self.all.count
-#  end
+  def self.count
+    self.all.count
+  end
 
   def add_song(song)
     @songs << song
     song.artist = self
   end
-#  def add_songs(songs)
-#    songs.each { |song| add_song(song) }
-#  end
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
 
 #
 end
