0ba7043860410af758d8c9fb1bb6dc3c4f4a012f
Kwane Smith <smithkwane6@gmail.com>
2020-02-22 07:41:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 class Song
 
-  extend Findable 
+  extend Findable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable 
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
