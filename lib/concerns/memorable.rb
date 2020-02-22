b88e6d05a0237fcd0d460d402263524bc9b89e4e
Kwane Smith <smithkwane6@gmail.com>
2020-02-22 07:46:45 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,15 +1,20 @@
 module Memorable
-  def self.reset_all
-    self.all.clear
+
+  module ClassMethods
+
+    def reset_all
+      all.clear
+    end
+
+    def count
+      all.count
+    end
   end
-end
-  def self.count
-    self.all.count
-  end
-end
-module InstanceMethods
-  def initialize
+
+  module InstanceMethods
+    def initialize
       self.class.all << self
     end
   end
-end
+
+end 
